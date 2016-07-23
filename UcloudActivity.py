576252f5b1fd053b1f83fac489c6f36c88c2bbed
denon <denonw@foxmail.com>
2016-07-23 13:38:43 +0800

    first commit

---
+++ UcloudActivity.py
@@ -0,0 +1,34 @@
+import requests
+
+__author__ = 'denonw'
+# 顺便问问你们还招人吗
+
+
+
+def get_text():
+    return requests.get("http://106.75.28.160/UCloud.txt").text
+
+
+def count_ucanuup(txt):
+    list = txt.split("UCanUup")
+    return len(list) - 1
+
+
+def count_by_index(txt):
+    index = -1
+    count = 0
+    while True:
+        try:
+            index += 1
+            index = txt.index("UCanUup", index)
+            count += 1
+        except:
+            break
+    return count
+
+if __name__ == "__main__":
+    txt = get_text()
+    total = count_ucanuup(txt)
+    total2 = count_by_index(txt)
+    print(total)
+    print(total2)
